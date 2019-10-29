2deb482f86508235372aaf3aa6a7feaa11b2095f
Tim Schweizer <schweize@ulaberta.ca>
2019-10-29 13:40:06 -0600

    Added Template

---
+++ Code/app/src/main/java/com/example/myapplication/MoodHistory.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * This class is dedicated to the activity_mood_history view and will handle that view's needs
+ */
+public class MoodHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_history);
+    }
+}
