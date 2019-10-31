a10cf91235ee3af79efb0e61b60ce0169b6ced18
Tim Schweizer <schweize@ulaberta.ca>
2019-10-31 15:33:57 -0600

    MoodHistory Crashing

--- Code/app/src/main/java/com/example/myapplication/MoodHistory.java
+++ Code/app/src/main/java/com/example/myapplication/MoodHistory.java
@@ -2,25 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import java.sql.Time;
-import java.util.ArrayList;
-import java.util.Date;
-
-/**
- * This class is dedicated to the activity_mood_history view and will handle that view's needs
- */
 public class MoodHistory extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_history);
-
     }
 }
