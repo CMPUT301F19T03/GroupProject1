e7675e73fadcf9ec16ecad18367822cdc37f1708
Tim Schweizer <schweize@ulaberta.ca>
2019-11-04 17:32:02 -0700

    Mood Delete need to get noew one

--- Code/app/src/main/java/com/example/myapplication/MoodHistory.java
+++ Code/app/src/main/java/com/example/myapplication/MoodHistory.java
@@ -24,28 +24,7 @@ public class MoodHistory extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_history);
-        moodArrayList= new ArrayList<Mood>();
-        // Test code to insure the list view is working
-        //Does sort need to be implemented in the mood class? How to create a custom sort?
-        moodHistory=findViewById(R.id.mood_history);
-        //Date date= new Date(2020,11,13);
-        Date date = new Date(2021,10,16);
-        Time time= new Time(23,22,21);
-        int image= R.drawable.neutral;
-        Location l = new Location("Hell");
-        State s = new State("black","white");
-        Mood mood=new Mood(date,time,l,s,"hi","hi",image);
-        moodArrayList.add(mood);
-        Date date2= new Date(2021,10,16);
-        Time time2= new Time(23,23,11);
-        int image2= R.drawable.great;
-        Location l2 = new Location("Hell");
-        State s2 = new State("black","white");
-        Mood mood2=new Mood(date2,time2,l2,s2,"hi","hi",image2);
-        moodArrayList.add(mood2);
-        Collections.sort(moodArrayList, new MoodComparator());
-        moodArrayAdapter = new CustomList(this,moodArrayList);
-        moodHistory.setAdapter(moodArrayAdapter);
+        //How to sort the array list:Collections.sort(moodArrayList, new MoodComparator());
 
     }
 }
